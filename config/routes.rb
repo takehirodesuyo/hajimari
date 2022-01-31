0311a922c275217ba78510310f1ddfa371bef127
takehirodesuyo <takehiro.kt106@gmail.com>
2022-01-31 22:59:24 +0900

    sing up 機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
